6af7d1f51907fe2b2099aa61a2f60240c4acd170
Joakim <j-pf@live.se>
2020-12-03 18:55:20 +0100

    La till midsommarafton från 5år bakåt

--- logic.js
+++ logic.js
@@ -3,6 +3,7 @@ document.addEventListener("onload", initsite())
 
 function initsite() {
     getWorkFreeDays()
+    getFiveYears()
 }   
 
 async function makeReq(path) {
@@ -38,5 +39,54 @@ function createHtml(childElement, innerText, parentElement) {
     
 }
 
+async function getFiveYears() {
+    let yearsToGet = [2015, 2016, 2017, 2018, 2019, 2020]
+    let years = []
+    
+    for (const year of yearsToGet) {
+        years = years.concat(await getDays(year))
+    }
+    for(let i = 0; i < years.length; i++) {
+      /*   console.log("firstloop = ", years[i]) */
+        
+        let daysOnYear = years[i].dagar //Varför blir years[i].dagar undefined om jag försöker loopa det direkt utan variable? 
+        
+        for(let i = 0; i < daysOnYear.length; i++) {
+            if(daysOnYear[i].helgdag === "Midsommarafton")
+            console.log(daysOnYear[i])
+        }
+    }
+    
 
     
+
+}
+
+
+/* function getMidsummer(days) {
+    for(let i = 0; i < days.dagar.length; i++) {
+        if(days.dagar[i].helgdag === "Midsommarafton"){
+            console.log(days)
+        }
+    }
+    
+} */
+
+
+
+/* async function getFiveYears() {
+    let y15 = await getDays(2020)
+    
+    for(let i = 0; i < y15.dagar.length; i++) {
+        if(y15.dagar[i].helgdag === "Midsommarafton"){
+    
+            console.log(y15.dagar[i]) 
+    }
+
+     
+
+
+
+    }
+
+} */
